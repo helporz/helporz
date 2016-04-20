547952159183c34e00a47629a363b51bfd7df495
binfeng <335311125@qq.com>
2016-04-21 06:47:10 +0800

    修复切换tab页后滚动条无法复位问题。

--- www/modules/main/me/me.controller.js
+++ www/modules/main/me/me.controller.js
@@ -6,7 +6,7 @@
   'use strict';
 
   angular.module('main.me')
-    .controller('mainMeCtrl', ['$state', '$scope', mainMeCtrl])
+    .controller('mainMeCtrl', ['$state', '$scope','$ionicScrollDelegate', mainMeCtrl])
     //.directive('tabLogic', function(){
     //  return function(scope, element, attr){
     //    if(attr.navView == 'inactive'){
@@ -18,9 +18,10 @@
     //}
     //);
 
-  function mainMeCtrl($state, $scope) {
+  function mainMeCtrl($state, $scope,$ionicScrollDelegate) {
     var vm = $scope.vm = {};
 
+    vm.meScroll = $ionicScrollDelegate.$getByHandle('meScroll');
     vm.edit = function () {
       $state.go('main.me.friend');
     }
@@ -37,6 +38,10 @@
       console.log('drag' + ':' + ev);
     }
 
+    vm.onChangeTab = function(href) {
+      vm.meScroll.scrollTop();
+      $state.go(href);
+    }
 
   }
 
