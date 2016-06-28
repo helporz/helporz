c9ee1460b7af6e20fea2d04940e7bda79851b34a
binfeng <335311125@qq.com>
2016-06-28 15:22:56 +0800

    在userNetService中增加封装修改个人信息接口

--- www/js/services/user.netservice.js
+++ www/js/services/user.netservice.js
@@ -6,9 +6,10 @@
   'use strict';
 
   angular.module('com.helporz.user.netservice', ['com.helporz.utils.service']).factory('userNetService', ['$q', '$log', 'httpBaseService',
-    'errorCodeService', 'httpErrorCodeService', UserNetServiceFactoryFn])
+    'errorCodeService', 'httpErrorCodeService','uploadService','userLoginInfoService' ,UserNetServiceFactoryFn])
 
-  function UserNetServiceFactoryFn($q, $log, httpBaseService, errorCodeService, httpErrorCodeService) {
+  function UserNetServiceFactoryFn($q, $log, httpBaseService, errorCodeService, httpErrorCodeService,uploadService,
+                                   userLoginInfoService) {
 
     // cache
     var _cache = {
@@ -25,7 +26,7 @@
         userinfo: phoneNo,
         smscode: smsCode
       }
-      return httpBaseService.postForPromise("/user/verify_sms",data);
+      return httpBaseService.postForPromise("/user/verify_sms", data);
     }
 
     var _loginByTicket = function (ticket, sign) {
@@ -87,15 +88,85 @@
       });
     };
 
+    // 1为男，2为女
+    var updateGender = function (gender) {
+      var param = {
+        gender: gender,
+      }
+      return httpBaseService.postForPromise('/user/self/update_gender', param);
+    }
+
+    var updateOrg = function (orgId) {
+      var param = {
+        orgId:orgId,
+      }
+      return httpBaseService.postForPromise('/user/self/update_org',param);
+    }
+
+    var updateDormitory = function(dormitory) {
+      var param = {
+        dormitory:dormitory,
+      }
+      return httpBaseService.postForPromise('/user/self/update_dormitory',param);
+    }
+
+    var updateDepartment = function(department) {
+      var param = {
+        department:department,
+      }
+      return httpBaseService.postForPromise('/user/self/update_department',param);
+    }
+
+    var updateHometown = function(hometown) {
+      var param = {
+        hometown:hometown,
+      }
+
+      return httpBaseService.postForPromise('/user/self/update_hometown',param);
+    }
+
+    var updateNickname = function(nickname) {
+      var param = {
+        nickname:nickname,
+      };
+
+      return httpBaseService.postForPromise('/user/self/update_nickname',param);
+    }
+
+    var updateAvatar = function(nativeUrl) {
+      var headers = {
+        Connection: "close",
+        'x-login-key': userLoginInfoService.getLoginTicket()
+      }
+      return uploadService.uploadImgFile(nativeUrl, appConfig.API_SVC_URL +
+        '/user/self/upload_avatar', headers);
+
+    }
+
+    var uploadIDCardPhoto = function(nativeUrl) {
+      var headers = {
+        Connection: "close",
+        'x-login-key': userLoginInfoService.getLoginTicket()
+      }
+      return uploadService.uploadImgFile(nativeUrl, appConfig.API_SVC_URL +
+        '/user/self/id_card_photo_ex', headers);
+    }
+
     return {
-      login:_login,
+      login: _login,
       loginByTicket: _loginByTicket,
       logout: _logout,
       checkUpdatePackage: _checkUpdatePackage,
       getSelfInfo: _getSelfInfo,
       getUserInfo: _getUserInfo,
       getSelfInfoForPromise: _getSelfInfoForPromise,
-
+      updateGender: updateGender,
+      updateOrg: updateOrg,
+      updateDormitory:updateDormitory,
+      updateHometown:updateHometown,
+      updateNickname:updateNickname,
+      uploadAvatar:uploadAvatar,
+      uploadIDCardPhoto:uploadIDCardPhoto,
       cache: _cache
     };
   }
